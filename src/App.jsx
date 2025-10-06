94e6f0a9f889eac0d0ea742b2b3c408358644be0
msharif123 <mohamed.sharif.abdiwahab@gmail.com>
2025-10-06 23:37:51 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Favorite from "./components/Favorite/Favorite";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 // import Search from "./components/Search/Search";
-import Weather from "./components/weather/Weather.jsx";
+import Weather from "./components/weather_fix/Weather.jsx";
 import './App.css'; // Make sure this is imported
 
 const App = () => {
